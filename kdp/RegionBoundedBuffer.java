e1b71b6626140961d2e57b8ade82816a1035d2d7
djolobolonjez <djordjem@outlook.com>
2022-12-14 18:23:43 +0100

    Added bounded buffer with critical region

---
+++ kdp/RegionBoundedBuffer.java
@@ -0,0 +1,48 @@
+package kdp;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RegionBoundedBuffer<T> implements BoundedBuffer<T> {
+	
+	private Queue<T> buffer = new LinkedList<>();
+
+	@Override
+	public void put(T data) {
+		
+		synchronized (buffer) {
+			buffer.add(data);
+			buffer.notify();
+		}
+		
+	}
+
+	@Override
+	public T get() {
+		T data;
+		
+		synchronized (buffer) {
+			while (buffer.size() == 0) {
+				try {
+					buffer.wait();
+				} catch (InterruptedException e) { }
+			}
+			
+			data = buffer.poll();
+		}
+		
+		return data;
+	}
+
+	@Override
+	public T pollFirst() {
+		T data;
+		
+		synchronized (buffer) {
+			data = buffer.poll();
+		}
+		
+		return data;
+	}
+	
+}
